03bd693e243c219c81c355be64e2eb97ce18bb97
Louise <yishinlin@outlook.com>
2021-08-03 21:41:37 +0800

    +Add simple Theme change
    click and toggle to change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 //import './App.css';
 //1.引入
 import styled from "@emotion/styled";
+import { useTheme, ThemeProvider, withTheme } from "@emotion/react"; 
 
 // 載入圖示
 import { ReactComponent as DayCloudyIcon } from "./images/day-cloudy.svg";
@@ -9,9 +10,34 @@ import { ReactComponent as AirFlowIcon } from "./images/airFlow.svg";
 import { ReactComponent as RainIcon } from "./images/rain.svg";
 import { ReactComponent as RefreshIcon } from "./images/refresh.svg";
 
-//2.定義帶有styled 的component
+
+//定義主題
+const theme = {
+  light: {
+    backgroundColor: '#ededed',
+    foregroundColor: '#f9f9f9',
+    boxShadow: '0 1px 3px 0 #999999',
+    titleColor: '#212121',
+    temperatureColor: '#757575',
+    textColor: '#828282',
+  },
+  dark: {
+    backgroundColor: '#1F2022',
+    foregroundColor: 'blue',
+    boxShadow:
+      '0 1px 4px 0 rgba(12, 12, 13, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.7)',
+    titleColor: '#f9f9fa',
+    temperatureColor: '#dddddd',
+    textColor: '#cccccc',
+  },
+};
+
+
+
+//定義帶有styled 的component
 const Container = styled.div`
-  background-color: #ededed;
+ /*在 Styled Component 中可以透過 Props 取得對的顏色 */
+  background-color:  ${({ theme }) => theme.backgroundColor};
   height: 100%;
   display: flex;
   align-items: center;
@@ -20,19 +46,20 @@ const Container = styled.div`
 const WeatherCard = styled.div`
   position: relative;
   min-width: 360px;
-  box-shadow: 0 1px 3px 0 #999999;
-  background-color: #f9f9f9;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  background-color: ${({ theme }) => theme.foregroundColor}; 
   box-sizing: border-box;
   padding: 30px 15px;
+  cursor:pointer;
 `;
 const Location = styled.div`
   font-size: 28px;
-  color: #212121;
+  color: ${({ theme }) => theme.titleColor};
   margin-bottom: 20px;
 `;
 const Description = styled.div`
   font-size: 16px;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   margin-bottom: 30px;
 `;
 
@@ -44,7 +71,7 @@ const CurrentWeather = styled.div`
 `;
 
 const Temperature = styled.div`
-  color: #757575;
+  color: ${({ theme }) => theme.temperatureColor};
   font-size: 96px;
   font-weight: 300;
   display: flex;
@@ -60,7 +87,7 @@ display: flex;
 align-items: center;
 font-size: 16x;
 font-weight: 300;
-color: #828282;
+color: ${({ theme }) => theme.textColor};
 margin-bottom: 20px;
 svg {
   width: 25px;
@@ -75,7 +102,7 @@ display: flex;
 align-items: center;
 font-size: 16x;
 font-weight: 300;
-color: #828282;
+color: ${({ theme }) => theme.textColor};
 svg {
   width: 25px;
   height: auto;
@@ -94,7 +121,7 @@ bottom: 15px;
 font-size: 12px;
 display: inline-flex;
 align-items: flex-end;
-color: #828282;
+color: ${({ theme }) => theme.textColor};
 svg {
   margin-left: 10px;
   width: 15px;
@@ -104,9 +131,19 @@ svg {
 `;
 
 function App() {
+  const [currentTheme, setCurrentTheme] = useState('light')
+  const [isClicked, setIsClick] = useState(false);
+
+  useEffect(() => {
+    setCurrentTheme(isClicked === false ? 'light' : 'dark');
+  }, [isClicked]);
+
   return (
-    <Container>
-      <WeatherCard>
+    
+//把所有會用到主題配色的部分都包在 ThemeProvider 內， 並透過 theme 這個 props 傳入深色主題
+    <ThemeProvider theme={theme[currentTheme]} > 
+    <Container >
+      <WeatherCard onClick={()=>setIsClick(!isClicked)}>
         <Location>台北市</Location>
         <Description>多雲時晴</Description>
         <CurrentWeather>
@@ -126,6 +163,7 @@ function App() {
         </Refresh>
       </WeatherCard>
     </Container>
+    </ThemeProvider > 
   );
 }
 
