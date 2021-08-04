8374045de96f833eea310fbebf203590bc922b8d
Louise <yishinlin@outlook.com>
2021-08-04 22:22:43 +0800

    + fetch OpenData API, useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 //import './App.css';
 //1.引入
 import styled from "@emotion/styled";
-import { useTheme, ThemeProvider, withTheme } from "@emotion/react"; 
+import { ThemeProvider } from "@emotion/react";
+import dayjs from "dayjs";
 
 // 載入圖示
 import { ReactComponent as DayCloudyIcon } from "./images/day-cloudy.svg";
@@ -130,40 +131,80 @@ svg {
 }
 `;
 
+const AUTH = "CWB-86B7E04A-22F9-41AF-BD59-CE75D9E5658F";
+const LOCATION_NAME = '臺北';
+
 function App() {
-  const [currentTheme, setCurrentTheme] = useState('light')
+  const [currentTheme, setCurrentTheme] = useState('light');
+  const [currentWeather, setCurrentWeather] = useState(
+    {
+      locationName: ' 臺北市 ',
+      description: ' 多雲時晴 ',
+      windSpeed: 1.1,
+      temperature: 22.9,
+      rainPossibility: 48.3,
+      observationTime: '2020-12-12 22:10:00',
+    }
+
+  );
   const [isClicked, setIsClick] = useState(false);
 
+  const fetchCurrentWeather = () => {
+    fetch(`http://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=${AUTH}&locationName=${LOCATION_NAME}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        //STEP 1：定義 `locationData` 把回傳的資料中會用到的部分取出來 
+        const locationData = data.records.location[0];
+        //STEP 2：過濾資料
+
+        const reducer = (neededElements, item) => { if (['WDSD', 'TEMP'].includes(item.elementName)) { neededElements[item.elementName] = item.elementValue; } return neededElements; }
+        const weatherElements = locationData.weatherElement.reduce(reducer, {});
+
+        // STEP 3： 更新 React 元件中的資料狀態 
+        setCurrentWeather({ 
+          observationTime: locationData.time.obsTime, 
+          locationName: locationData.locationName, 
+          temperature: weatherElements.TEMP, 
+          windSpeed: weatherElements.WDSD, 
+          description: ' 多雲時晴 ', 
+          rainPossibility: 60, });
+      })
+
+  }
+
   useEffect(() => {
+    fetchCurrentWeather()
     setCurrentTheme(isClicked === false ? 'light' : 'dark');
   }, [isClicked]);
 
   return (
-    
-//把所有會用到主題配色的部分都包在 ThemeProvider 內， 並透過 theme 這個 props 傳入深色主題
-    <ThemeProvider theme={theme[currentTheme]} > 
-    <Container >
-      <WeatherCard onClick={()=>setIsClick(!isClicked)}>
-        <Location>台北市</Location>
-        <Description>多雲時晴</Description>
-        <CurrentWeather>
-          <Temperature>
-            23 <Celsius>°C</Celsius>
-          </Temperature>
-          <DayCloudy />
-        </CurrentWeather>
-        <AirFlow>
-          <AirFlowIcon /> 23 m/h
-        </AirFlow>
-        <Rain>
-          <RainIcon /> 48%
-        </Rain>
-        <Refresh>
-          最後觀測時間：上午 12:03 <RefreshIcon />
-        </Refresh>
-      </WeatherCard>
-    </Container>
-    </ThemeProvider > 
+
+    //把所有會用到主題配色的部分都包在 ThemeProvider 內， 並透過 theme 這個 props 傳入深色主題
+    <ThemeProvider theme={theme[currentTheme]} >
+      <Container >
+        <WeatherCard onClick={() => setIsClick(!isClicked)}>
+          <Location>{currentWeather.locationName}</Location>
+          <Description>{currentWeather.description}</Description>
+          <CurrentWeather>
+            <Temperature>
+              {Math.round(currentWeather.temperature)}  <Celsius>°C</Celsius>
+            </Temperature>
+            <DayCloudy />
+          </CurrentWeather>
+          <AirFlow>
+            <AirFlowIcon /> {currentWeather.windSpeed}  m/h
+          </AirFlow>
+          <Rain>
+            <RainIcon /> {currentWeather.rainPossibility} %
+          </Rain>
+          <Refresh onClick={fetchCurrentWeather}>
+            {/* JSX 中預設的空格最後在網頁 呈現時都會被過濾掉，因此如果你希望最後在頁面上元件與元件間是留有 空格的，就可以透過帶入「空字串」的方式來加入空格 */}
+            最後觀測時間：{new Intl.DateTimeFormat('zh-TW', { hour: 'numeric', minute: 'numeric', }).format(dayjs(currentWeather.observationTime))} {' '} <RefreshIcon />
+          </Refresh>
+        </WeatherCard>
+      </Container>
+    </ThemeProvider >
   );
 }
 
